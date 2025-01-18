e9877a38d971414ea90b2a66c874dda42abdf12b
Emmanuel <esiziba71@gmail.com>
2025-01-18 22:24:39 +0200

    fetching episodes using async

--- script.js
+++ script.js
@@ -6,13 +6,34 @@ const state = {
 const endpoint = " https://api.tvmaze.com/shows/82/episodes";
 
 const fetchFilms = async () => {
-  const response = await fetch(endpoint);
-  return await response.json();
-}; // Our async function returns a Promise
+  try {
+    document.getElementById("status").textContent = "Loading episodes, please wait...";
 
+    const response = await fetch(endpoint);
+
+    if(!response.ok){
+      throw new error("Failed to fetch episodes");
+    }
+    const data = await response.json();
+
+    document.querySelector("#status").textContent = "";
+
+    return data;
+  
+
+  
+}catch (error) {
+  document.getElementById("status").textContent = "Error fetching episodes. Please try again leter.";
+  alert("Error fetching episodes:", error);
+  return [];
+
+} // Our async function returns a Promise
+
+};
 fetchFilms().then((films) => {
   // When the fetchFilms Promise resolves, this callback will be called.
   state.films = films;
+
   render();
 });
 
